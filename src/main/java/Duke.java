7a1fc2bd7437bd8a0fae658e25e752ef9fc683b7
henryxuy <605513104@qq.com>
2021-01-10 21:41:49 +0800

    Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 
 public class Duke {
+    // String[] to save what the user has entered
+    private static String[] textList = new String[100];
+    private static int arrayCount = 0;
+
     public static void main(String[] args) {
 //        String logo = " ____        _        \n"
 //                + "|  _ \\ _   _| | _____ \n"
@@ -9,10 +13,44 @@ public class Duke {
 //                + "|____/ \\__,_|_|\\_\\___|\n";
 //        System.out.println("Hello from\n" + logo);
 
+        // Rm: Single Level of Abstraction Per method
         printGreeting();
-        echoAndExit();
+        addListAndExit();
 
     }
+
+    private static void addListAndExit(){
+        Scanner in = new Scanner(System.in);
+        String tempDialogue = "";
+        while(true){
+            tempDialogue = in.nextLine();
+            if(tempDialogue.equals("list")){
+                /* list out the previous tasks */
+                printList();
+            }
+            else if(tempDialogue.equals("bye")){
+                echoChat("Bye. Hope to see you again soon!");
+                break;
+            }
+            else {
+                /* add to list and show (happy path) */
+                textList[arrayCount++] = tempDialogue;
+                echoChat("added: " + tempDialogue);
+            }
+        }
+        
+        return;
+    }
+
+    private static void printList(){
+        System.out.println("____________________________________________________________");
+        for(int i = 0; i < arrayCount; i++) {
+            System.out.println((i + 1) + ". " + textList[i]);
+        }
+        System.out.println("____________________________________________________________");
+        return;
+    }
+
 
     private static void echoAndExit(){
         Scanner in = new Scanner(System.in);
