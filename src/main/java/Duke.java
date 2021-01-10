b26a0c6c161c025f71a0f675bbae7ec2fbd584b2
henryxuy <605513104@qq.com>
2021-01-10 22:47:51 +0800

    Level 3. Mark as Done (Change the String[] taskList into Task[])

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Duke {
     // String[] to save what the user has entered
-    private static String[] textList = new String[100];
+    private static Task[] taskList = new Task[100];
     private static int arrayCount = 0;
 
     public static void main(String[] args) {
@@ -32,37 +32,62 @@ public class Duke {
                 echoChat("Bye. Hope to see you again soon!");
                 break;
             }
+            else if(tempDialogue.startsWith("done")){
+                String[] splitString = tempDialogue.split("done ");
+                int taskIndex;
+
+                try {
+                    taskIndex = Integer.parseInt(splitString[1]) - 1;
+                }
+                catch (NumberFormatException ex){
+                    // if the string cannot be parsed successfully
+                    System.out.println("Invalid done string.");
+                    continue;
+                }
+
+                if(taskIndex < 0 || taskIndex >= arrayCount){
+                    System.out.println("Wrong done index.");
+                    continue;
+                }
+
+                taskList[taskIndex].isDone = true;
+                echoChat("Nice! I've marked this task as done: \n" +
+                        "[" + taskList[taskIndex].getStatusIcon() + "]" + taskList[taskIndex].description);
+            }
             else {
                 /* add to list and show (happy path) */
-                textList[arrayCount++] = tempDialogue;
+                taskList[arrayCount++] = new Task(tempDialogue);
                 echoChat("added: " + tempDialogue);
             }
         }
-        
+
         return;
     }
 
     private static void printList(){
         System.out.println("____________________________________________________________");
         for(int i = 0; i < arrayCount; i++) {
-            System.out.println((i + 1) + ". " + textList[i]);
+            Task tempTask = taskList[i];
+            System.out.println((i + 1) + ". [" + tempTask.getStatusIcon() + "] " + tempTask.description);
+//            System.out.println("\u2713");
+//            System.out.println((i + 1) + " [" + "\u2713" +"] " + tempTask.description);
         }
         System.out.println("____________________________________________________________");
         return;
     }
 
 
-    private static void echoAndExit(){
-        Scanner in = new Scanner(System.in);
-        String tempDialogue = in.nextLine();
-        while(!tempDialogue.equals("bye")){
-            echoChat(tempDialogue);
-            tempDialogue = in.nextLine();
-        }
-        echoChat("Bye. Hope to see you again soon!");
-
-        return;
-    }
+//    private static void echoAndExit(){
+//        Scanner in = new Scanner(System.in);
+//        String tempDialogue = in.nextLine();
+//        while(!tempDialogue.equals("bye")){
+//            echoChat(tempDialogue);
+//            tempDialogue = in.nextLine();
+//        }
+//        echoChat("Bye. Hope to see you again soon!");
+//
+//        return;
+//    }
 
     private static void printGreeting(){
         System.out.println("____________________________________________________________");
@@ -79,3 +104,8 @@ public class Duke {
 
 
 }
+
+
+
+
+
