ccee0166bb9b4f5646377273519f6e018c3a9e97
henryxuy <605513104@qq.com>
2021-01-11 19:05:25 +0800

    Level 5. Handle Errors, Level 6. Delete

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,9 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     // String[] to save what the user has entered
-    private static Task[] taskList = new Task[100];
+    private static ArrayList<Task> taskList = new ArrayList<>();
     private static int arrayCount = 0;
 
     public static void main(String[] args) {
@@ -17,35 +18,38 @@ public class Duke {
         printGreeting();
         addListAndExit();
 
-
     }
 
     private static void addListAndExit(){
         Scanner in = new Scanner(System.in);
         String tempDialogue = "";
         while(true){
-            tempDialogue = in.nextLine();
-            if(tempDialogue.equals("list")){
-                /* list out the previous tasks */
-                printList();
+            try {
+                tempDialogue = in.nextLine();
+                if (tempDialogue.equals("list")) {
+                    /* list out the previous tasks */
+                    printList();
+                } else if (tempDialogue.equals("bye")) {
+                    echoChat("Bye. Hope to see you again soon!");
+                    break;
+                } else if (tempDialogue.startsWith("done")) {
+                    doneTask(tempDialogue);
+                } else if (tempDialogue.startsWith("delete")) {
+                    deleteTask(tempDialogue);
+                } else {
+                    /* add to list and show */
+                    addTask(tempDialogue);
+                }
             }
-            else if(tempDialogue.equals("bye")){
-                echoChat("Bye. Hope to see you again soon!");
-                break;
-            }
-            else if(tempDialogue.startsWith("done")){
-                doneTask(tempDialogue);
-            }
-            else {
-                /* add to list and show */
-                addTask(tempDialogue);
+            catch (DukeException ex){
+                echoChat(ex.toString());
             }
         }
 
         return;
     }
 
-    private static void doneTask(String tempDialogue) {
+    private static void doneTask(String tempDialogue) throws DukeException {
         String[] splitString = tempDialogue.split("done ");
         int taskIndex;
 
@@ -54,63 +58,171 @@ public class Duke {
         }
         catch (NumberFormatException ex){
             // if the string cannot be parsed successfully
-            System.out.println("Invalid done string.");
-            return;
+            throw new DukeException(DukeExceptionType.WRONG_DONE_INDEX);
         }
 
         if(taskIndex < 0 || taskIndex >= arrayCount){
-            System.out.println("Wrong done index.");
-            return;
+            throw new DukeException(DukeExceptionType.WRONG_DONE_INDEX);
         }
 
-        taskList[taskIndex].isDone = true;
+        taskList.get(taskIndex).isDone = true;
         echoChat("Nice! I've marked this task as done: \n" +
-                "[" + taskList[taskIndex].getStatusIcon() + "]" + taskList[taskIndex].description);
+                taskList.get(taskIndex));
     }
 
 
     private static void addTask(String taskString){
-        // parse the task
         try {
-            String[] timeSplitString = taskString.split(" /");
-            int spaceIndex = timeSplitString[0].indexOf(" ");
-            String taskType = timeSplitString[0].substring(0, spaceIndex);
-            String taskDescription = timeSplitString[0].substring(spaceIndex + 1);
-            String time = "";
-
-            // add the task
-            switch (taskType) {
-                case "todo":
-                    taskList[arrayCount++] = new Todo(taskDescription);
-                    break;
-                case "deadline":
-                    time = timeSplitString[1];
-                    taskList[arrayCount++] = new Deadline(taskDescription, time);
-                    break;
-                case "event":
-                    time = timeSplitString[1];
-                    taskList[arrayCount++] = new Event(taskDescription, time);
-                    break;
-                default:
-                    throw new Exception("Invalid task type");
+            if (taskString.startsWith("todo")) {
+                addTodo(taskString);
             }
-            echoChat("Got it. I've added this task:\n" + taskList[arrayCount-1] +
-                    "\nNow you have " + arrayCount + " tasks in the list.");
+            else if (taskString.startsWith("deadline")) {
+                addDeadline(taskString);
+            }
+            else if (taskString.startsWith("event")) {
+                addEvent(taskString);
+            }
+            else {
+                throw new DukeException(DukeExceptionType.MEANINGLESS_INPUT);
+            }
+        }
+        catch (DukeException ex){
+            echoChat(ex.toString());
         }
 
-        catch (Exception ex){
-            System.out.println("Invalid task input");
-        }
     }
 
 
+    private static void addTodo(String todoString) throws DukeException{
+        if(todoString.endsWith("todo")){
+            throw new DukeException(DukeExceptionType.EMPTY_TODO);
+        }
+
+        int spaceIndex = todoString.indexOf(" ");
+//        String taskType = todoString.substring(0, spaceIndex);
+        String taskDescription = todoString.substring(spaceIndex + 1);
+
+        taskList.add(new Todo(taskDescription));
+        arrayCount++;
+
+        echoChat("Got it. I've added this task:\n" + taskList.get(arrayCount-1) +
+                    "\nNow you have " + arrayCount + " tasks in the list.");
+    }
+
+    private static void addDeadline(String deadlineString) throws DukeException{
+        if(deadlineString.endsWith("deadline")){
+            throw new DukeException(DukeExceptionType.EMPTY_DEADLINE);
+        }
+        if(!deadlineString.contains(" /")){
+            throw new DukeException(DukeExceptionType.NO_TIME_DEADLINE);
+        }
+
+        String[] timeSplitString = deadlineString.split(" /");
+        int spaceIndex = timeSplitString[0].indexOf(" ");
+//        String taskType = timeSplitString[0].substring(0, spaceIndex);
+        String taskDescription = timeSplitString[0].substring(spaceIndex + 1);
+        String time = "";
+
+        time = timeSplitString[1];
+        taskList.add(new Deadline(taskDescription, time));
+        arrayCount++;
+
+        echoChat("Got it. I've added this task:\n" + taskList.get(arrayCount-1) +
+                "\nNow you have " + arrayCount + " tasks in the list.");
+    }
+
+    private static void addEvent(String eventString) throws DukeException{
+        if(eventString.endsWith("event")){
+            throw new DukeException(DukeExceptionType.EMPTY_EVENT);
+        }
+        if(!eventString.contains(" /")){
+            throw new DukeException(DukeExceptionType.NO_TIME_EVENT);
+        }
+
+        String[] timeSplitString = eventString.split(" /");
+        int spaceIndex = timeSplitString[0].indexOf(" ");
+//        String taskType = timeSplitString[0].substring(0, spaceIndex);
+        String taskDescription = timeSplitString[0].substring(spaceIndex + 1);
+        String time = "";
+
+        time = timeSplitString[1];
+        taskList.add(new Event(taskDescription, time));
+        arrayCount++;
+
+        echoChat("Got it. I've added this task:\n" + taskList.get(arrayCount-1) +
+                "\nNow you have " + arrayCount + " tasks in the list.");
+    }
+
+
+
+//    private static void addTaskOld(String taskString){
+//        // parse the task
+//        try {
+//            String[] timeSplitString = taskString.split(" /");
+//            int spaceIndex = timeSplitString[0].indexOf(" ");
+//            String taskType = timeSplitString[0].substring(0, spaceIndex);
+//            String taskDescription = timeSplitString[0].substring(spaceIndex + 1);
+//            String time = "";
+//
+//            // add the task
+//            switch (taskType) {
+//                case "todo":
+//                    taskList.add(new Todo(taskDescription));
+//                    arrayCount++;
+//                    break;
+//                case "deadline":
+//                    time = timeSplitString[1];
+//                    taskList.add(new Deadline(taskDescription, time));
+//                    arrayCount++;
+//                    break;
+//                case "event":
+//                    time = timeSplitString[1];
+//                    taskList.add(new Event(taskDescription, time));
+//                    arrayCount++;
+//                    break;
+//                default:
+//                    throw new Exception("Invalid task type");
+//            }
+//            echoChat("Got it. I've added this task:\n" + taskList.get(arrayCount-1) +
+//                    "\nNow you have " + arrayCount + " tasks in the list.");
+//        }
+//
+//        catch (Exception ex){
+//            System.out.println("Invalid task input");
+//        }
+//    }
+
+
+    private static void deleteTask(String tempDialogue) throws DukeException {
+        String[] splitString = tempDialogue.split("delete ");
+        int taskIndex;
+
+        try {
+            taskIndex = Integer.parseInt(splitString[1]) - 1;
+        }
+        catch (NumberFormatException ex){
+            // if the string cannot be parsed successfully
+            throw new DukeException(DukeExceptionType.WRONG_DELETE_INDEX);
+        }
+
+        if(taskIndex < 0 || taskIndex >= arrayCount){
+            throw new DukeException(DukeExceptionType.WRONG_DELETE_INDEX);
+        }
+        Task tempTask = taskList.get(taskIndex);
+        taskList.remove(taskIndex);
+        arrayCount--;
+        echoChat("Noted. I've removed this task: \n" +
+                tempTask +
+                "\n Now you have " + arrayCount + " tasks in the list.");
+
+    }
 
 
     private static void printList(){
         System.out.println("____________________________________________________________");
         System.out.println("Here are the tasks in your list:");
         for(int i = 0; i < arrayCount; i++) {
-            Task tempTask = taskList[i];
+            Task tempTask = taskList.get(i);
             System.out.println((i + 1) + ". " + tempTask);
 //            System.out.println("\u2713");
 //            System.out.println((i + 1) + " [" + "\u2713" +"] " + tempTask.description);
---
+++ src/main/java/DukeException.java
@@ -0,0 +1,58 @@
+public class DukeException extends Exception{
+//    private int exceptionType = 0;
+    private DukeExceptionType exceptionType;
+    private String description = "";
+
+//    // use const to denote exception types
+//    public static final int MEANINGLESS_INPUT = 1;
+//    public static final int EMPTY_TODO = 2;
+//    public static final int EMPTY_DEADLINE = 3;
+//    public static final int EMPTY_EVENT = 4;
+//    public static final int NO_TIME_DEADLINE = 5;
+//    public static final int NO_TIME_EVENT = 6;
+//    public static final int WRONG_DONE_INDEX = 7;
+
+    public DukeException(DukeExceptionType type){
+        this.exceptionType = type;
+        switch (this.exceptionType){
+            case MEANINGLESS_INPUT:
+                this.description = "OOPS!!! I'm sorry, but I don't know what that means :-(";
+                break;
+            case EMPTY_TODO:
+                this.description = "OOPS!!! The description of a todo cannot be empty.";
+                break;
+            case EMPTY_DEADLINE:
+                this.description = "OOPS!!! The description of a deadline cannot be empty.";
+                break;
+            case EMPTY_EVENT:
+                this.description = "OOPS!!! The description of a event cannot be empty.";
+                break;
+            case NO_TIME_DEADLINE:
+                this.description = "OOPS!!! The time of a deadline cannot be empty.";
+                break;
+            case NO_TIME_EVENT:
+                this.description = "OOPS!!! The time of a event cannot be empty.";
+                break;
+            case WRONG_DONE_INDEX:
+                this.description = "OOPS!!! The done index is wrong.";
+                break;
+            case WRONG_DELETE_INDEX:
+                this.description = "OOPS!!! The delete index is wrong.";
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public String toString(){
+        return this.description;
+    }
+
+}
+
+enum DukeExceptionType{
+    MEANINGLESS_INPUT, EMPTY_TODO, EMPTY_DEADLINE, EMPTY_EVENT,
+    NO_TIME_DEADLINE, NO_TIME_EVENT, WRONG_DONE_INDEX, WRONG_DELETE_INDEX
+}
+
