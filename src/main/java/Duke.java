b0c409934e41d046bfbd0a8d77757662fdc72dbc
henryxuy <605513104@qq.com>
2021-01-11 20:37:18 +0800

    Level 7. Save. Save the task list in ./data/taskList.txt format is like: // save format: todo | 1 | read book // deadline | 0 | return book | June 6th

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -5,6 +7,8 @@ public class Duke {
     // String[] to save what the user has entered
     private static ArrayList<Task> taskList = new ArrayList<>();
     private static int arrayCount = 0;
+    // relative path to save the file
+    private final static String LIST_PATH = "./data/taskList.txt";
 
     public static void main(String[] args) {
 //        String logo = " ____        _        \n"
@@ -15,6 +19,7 @@ public class Duke {
 //        System.out.println("Hello from\n" + logo);
 
         // Rm: Single Level of Abstraction Per method
+
         printGreeting();
         addListAndExit();
 
@@ -30,6 +35,8 @@ public class Duke {
                     /* list out the previous tasks */
                     printList();
                 } else if (tempDialogue.equals("bye")) {
+                    /* save the task list when we exit */
+                    saveTaskList();
                     echoChat("Bye. Hope to see you again soon!");
                     break;
                 } else if (tempDialogue.startsWith("done")) {
@@ -89,8 +96,69 @@ public class Duke {
         catch (DukeException ex){
             echoChat(ex.toString());
         }
-
     }
+
+    private static void saveTaskList() {
+        File file = new File(LIST_PATH);
+        File fileDirectory = new File(file.getParent());
+        boolean directoryCreated = false;
+        boolean fileDeleted = true;
+        if(file.isDirectory()){
+            // directory is not valid to write into
+            System.out.println("The file path is not valid.");
+            System.exit(1);
+        }
+        if(!fileDirectory.exists()){
+            // make the directory if it does not exist
+            directoryCreated = fileDirectory.mkdirs();
+        }
+
+        if(file.exists()){
+            // delete the file if it exists
+            fileDeleted = file.delete();
+        }
+
+        try(java.io.PrintWriter output = new java.io.PrintWriter(file);)
+        {
+            // write to the file
+            for(Task task: taskList){
+                // save format: todo | 1 | read book
+                // deadline | 0 | return book | June 6th
+                if(task instanceof Todo) {
+                    Todo todoTask = (Todo) task;
+                    int isDone = todoTask.isDone ? 1 : 0;
+                    String taskString = String.format("todo | %d | %s", isDone, todoTask.description);
+                    output.println(taskString);
+                }
+                else if(task instanceof Deadline){
+                    Deadline deadlineTask = (Deadline) task;
+                    int isDone = deadlineTask.isDone ? 1 : 0;
+                    String taskString = String.format("deadline | %d | %s | %s", isDone, deadlineTask.description,
+                            deadlineTask.byTime);
+                    output.println(taskString);
+                }
+                else if(task instanceof Event){
+                    Event eventTask = (Event) task;
+                    int isDone = eventTask.isDone ? 1 : 0;
+                    String taskString = String.format("event | %d | %s | %s", isDone, eventTask.description,
+                            eventTask.atTime);
+                    output.println(taskString);
+                }
+                else{
+                    // merely task
+                    int isDone = task.isDone ? 1 : 0;
+                    String taskString = String.format("task | %d | %s", isDone, task.description);
+                    output.println(taskString);
+                }
+            }
+            output.println("EOF");
+        }
+        catch (FileNotFoundException ex){
+            System.out.println("The file path is not valid.");
+            System.exit(1);
+        }
+    }
+
 
 
     private static void addTodo(String todoString) throws DukeException{
